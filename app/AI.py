90bad7dfe55d500b11820a4e062be41b7590cca3
Victor Casado <vcasado50@stuy.edu>
2025-04-01 11:25:34 -0400

    yayy

---
+++ app/AI.py
@@ -0,0 +1,13 @@
+from google import genai
+import sys
+
+def getGeminiResponse(key, prompt):
+
+	client = genai.Client(api_key=key)
+
+	response = client.models.generate_content(
+    		model="gemini-2.5-pro-exp-03-25", contents=prompt
+	)
+
+	return response.text
+
