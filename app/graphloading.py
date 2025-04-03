a433a1d20a870d0308433c0c052202d2faeb96f2
Mark Ma <markworldamc@gmail.com>
2025-04-03 11:44:59 -0400

    a

--- app/graphloading.py
+++ app/graphloading.py
@@ -1,3 +1,11 @@
 import csv
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
 
-
+df = pd.read_csv('./csvs/tesla_stocks.csv')
+# datetime.strptime('', '%Y-%m-%d')
+df['Date'].map(lambda x: datetime.strptime(x, '%Y-%m-%d'))
+print(df['Date'].head)
+df.plot(kind = 'scatter', x = 'Date', y = 'Open')
+plt.show()
