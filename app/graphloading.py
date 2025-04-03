ae4b8d026a5e2d6e84f71b55da01c0a2a6fd6736
Michelle <jzhu50@stuy.edu>
2025-04-03 12:08:53 -0400

    tsla_data

--- app/graphloading.py
+++ app/graphloading.py
@@ -1,35 +1,40 @@
 import csv
-import pandas as pd
-from datetime import datetime
-import matplotlib.pyplot as plt
+#import pandas as pd
+#from datetime import datetime
+#import matplotlib.pyplot as plt
 
-<<<<<<< HEAD
 def tsla_data():
-    data = []
+    data = {}
     with open('csvs/tesla_stocks.csv', 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            data.append(row)
-    print(data)
-    return data
+            date = row['Date']
+            stock_price = row['Open']
+            data[date] = stock_price
+    #print(data)
+    return data #returns a dictionary of dates and tesla stock prices
 
 def tweet_data():
-    data = []
+    data = {}
     with open('csvs/musk_quote_tweets.csv', 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            data.append(row)
+            date = row['Date']
+            # we need to filter the data by date
+            tweet = row['Tweet']
+            data[date] = tweet
     print(data)
     return data
 
-if __name__ == '__main__':
-    tsla_data()
-    #tweet_data()
-=======
+'''
 df = pd.read_csv('./csvs/tesla_stocks.csv')
 # datetime.strptime('', '%Y-%m-%d')
 df['Date'].map(lambda x: datetime.strptime(x, '%Y-%m-%d'))
 print(df['Date'].head)
 df.plot(kind = 'scatter', x = 'Date', y = 'Open')
 plt.show()
->>>>>>> 30cb22e2859cc886056dc4215c0bed9003910a8d
+'''
+
+if __name__ == '__main__':
+    tsla_data()
+    #tweet_data()
