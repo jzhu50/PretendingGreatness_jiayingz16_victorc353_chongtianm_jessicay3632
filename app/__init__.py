9b9c18526392d94c0c8e75eb2c69c4aec7bf699f
Your Name <jzhu50@stuy.edu>
2025-04-23 16:42:59 +0000

    um

--- app/__init__.py
+++ app/__init__.py
@@ -98,6 +98,8 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
 
+'''
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0', port='5006')
+'''
